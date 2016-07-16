ee27df438eeae07400872930dfc34750072288fb
nminhduc <nguyen.minh.duc@framgia.com>
2016-07-16 16:58:39 +0700

    1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 haha</div>
+                <div class="title">Laravel 5 hahaaddsasas</div>
             </div>
         </div>
     </body>
